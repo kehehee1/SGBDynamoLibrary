c0b9b8aa4adc9eeba5a423ef48b08e90877f8d27
kehehee1 <kehehee1@163.com>
2017-06-22 22:15:12 +0800

    工程上传
    创建工程

---
+++ StringOperator.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SGBDynamoLibrary
+{
+    public class StringOperator
+    {
+        /// <summary>
+        /// 将对象转换为Int32类型
+        /// </summary>
+        /// <param name="strValue">要转换的字符串</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static int ObjectToInt(object expression)
+        {
+            return ObjectToInt(expression, 0);
+        }
+
+        /// <summary>
+        /// 将对象转换为Int32类型
+        /// </summary>
+        /// <param name="strValue">要转换的字符串</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static int ObjectToInt(object expression, int defValue)
+        {
+            if (expression != null)
+                return StrToInt(expression.ToString(), defValue);
+
+            return defValue;
+        }
+
+        /// <summary>
+        /// 将对象转换为Int32类型,转换失败返回0
+        /// </summary>
+        /// <param name="str">要转换的字符串</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static int StrToInt(string str)
+        {
+            return StrToInt(str, 0);
+        }
+
+        /// <summary>
+        /// 将对象转换为Int32类型
+        /// </summary>
+        /// <param name="str">要转换的字符串</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static int StrToInt(string str, int defValue)
+        {
+            if (string.IsNullOrEmpty(str) || str.Trim().Length >= 11 || !Regex.IsMatch(str.Trim(), @"^([-]|[0-9])[0-9]*(\.\w*)?$"))
+                return defValue;
+
+            int rv;
+            if (Int32.TryParse(str, out rv))
+                return rv;
+
+            return Convert.ToInt32(StrToFloat(str, defValue));
+        }
+
+        /// <summary>
+        /// string型转换为float型
+        /// </summary>
+        /// <param name="strValue">要转换的字符串</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static float StrToFloat(object strValue, float defValue)
+        {
+            if ((strValue == null))
+                return defValue;
+
+            return StrToFloat(strValue.ToString(), defValue);
+        }
+
+        /// <summary>
+        /// string型转换为float型
+        /// </summary>
+        /// <param name="strValue">要转换的字符串</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static float ObjectToFloat(object strValue, float defValue)
+        {
+            if ((strValue == null))
+                return defValue;
+
+            return StrToFloat(strValue.ToString(), defValue);
+        }
+
+        /// <summary>
+        /// string型转换为float型
+        /// </summary>
+        /// <param name="strValue">要转换的字符串</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static float ObjectToFloat(object strValue)
+        {
+            return ObjectToFloat(strValue.ToString(), 0);
+        }
+
+        /// <summary>
+        /// string型转换为float型
+        /// </summary>
+        /// <param name="strValue">要转换的字符串</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static float StrToFloat(string strValue)
+        {
+            if ((strValue == null))
+                return 0;
+
+            return StrToFloat(strValue.ToString(), 0);
+        }
+
+        /// <summary>
+        /// string型转换为float型
+        /// </summary>
+        /// <param name="strValue">要转换的字符串</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static float StrToFloat(string strValue, float defValue)
+        {
+            if ((strValue == null) || (strValue.Length > 10))
+                return defValue;
+
+            float intValue = defValue;
+            if (strValue != null)
+            {
+                bool IsFloat = Regex.IsMatch(strValue, @"^([-]|[0-9])[0-9]*(\.\w*)?$");
+                if (IsFloat)
+                    float.TryParse(strValue, out intValue);
+            }
+            return intValue;
+        }
+
+        /// <summary>
+        /// 将对象转换为日期时间类型
+        /// </summary>
+        /// <param name="str">要转换的字符串</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static DateTime StrToDateTime(string str, DateTime defValue)
+        {
+            if (!string.IsNullOrEmpty(str))
+            {
+                DateTime dateTime;
+                if (DateTime.TryParse(str, out dateTime))
+                    return dateTime;
+            }
+            return defValue;
+        }
+
+        /// <summary>
+        /// 将对象转换为日期时间类型
+        /// </summary>
+        /// <param name="str">要转换的字符串</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static DateTime StrToDateTime(string str)
+        {
+            return StrToDateTime(str, DateTime.Now);
+        }
+
+        /// <summary>
+        /// 将对象转换为日期时间类型
+        /// </summary>
+        /// <param name="obj">要转换的对象</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static DateTime ObjectToDateTime(object obj)
+        {
+            return StrToDateTime(obj.ToString());
+        }
+
+        /// <summary>
+        /// 将对象转换为日期时间类型
+        /// </summary>
+        /// <param name="obj">要转换的对象</param>
+        /// <param name="defValue">缺省值</param>
+        /// <returns>转换后的int类型结果</returns>
+        public static DateTime ObjectToDateTime(object obj, DateTime defValue)
+        {
+            return StrToDateTime(obj.ToString(), defValue);
+        }
+
+        /// <summary>
+        /// 替换回车换行符为html换行符
+        /// </summary>
+        public static string StrFormat(string str)
+        {
+            string str2;
+
+            if (str == null)
+            {
+                str2 = "";
+            }
+            else
+            {
+                str = str.Replace("\r\n", "<br />");
+                str = str.Replace("\n", "<br />");
+                str2 = str;
+            }
+            return str2;
+        }
+
+        /// <summary>
+        /// 转换为简体中文
+        /// </summary>
+        //public static string ToSChinese(string str)
+        //{
+        //    return Strings.StrConv(str, VbStrConv.SimplifiedChinese, 0);
+
+        //}
+
+        /// <summary>
+        /// 转换为繁体中文
+        /// </summary>
+        //public static string ToTChinese(string str)
+        //{
+        //    return Strings.StrConv(str, VbStrConv.TraditionalChinese, 0);
+
+        //}
+
+
+        /// <summary>
+        /// 清除字符串数组中的重复项
+        /// </summary>
+        /// <param name="strArray">字符串数组</param>
+        /// <param name="maxElementLength">字符串数组中单个元素的最大长度</param>
+        /// <returns></returns>
+        public static string[] DistinctStringArray(string[] strArray, int maxElementLength)
+        {
+            Hashtable h = new Hashtable();
+
+            foreach (string s in strArray)
+            {
+                string k = s;
+                if (maxElementLength > 0 && k.Length > maxElementLength)
+                {
+                    k = k.Substring(0, maxElementLength);
+                }
+                h[k.Trim()] = s;
+            }
+
+            string[] result = new string[h.Count];
+
+            h.Keys.CopyTo(result, 0);
+
+            return result;
+        }
+
+        /// <summary>
+        /// 清除字符串数组中的重复项
+        /// </summary>
+        /// <param name="strArray">字符串数组</param>
+        /// <returns></returns>
+        public static string[] DistinctStringArray(string[] strArray)
+        {
+            return DistinctStringArray(strArray, 0);
+        }
+
+        /// <summary>
+        /// 判读是否是IP地址
+        /// </summary>
+        /// <param name="in_str"></param>
+        /// <returns></returns>
+        public static bool IsIPStr(string in_str)
+        {
+            IPAddress ip;
+            return IPAddress.TryParse(in_str, out ip);
+        }
+
+        /// <summary>
+        /// 判断是否是数字
+        /// </summary>
+        /// <param name="strNumber"></param>
+        /// <returns></returns>
+        public static bool IsNumber(string strNumber)
+        {
+
+            Regex objNotNumberPattern = new Regex("[^0-9.-]");
+            Regex objTwoDotPattern = new Regex("[0-9]*[.][0-9]*[.][0-9]*");
+            Regex objTwoMinusPattern = new Regex("[0-9]*[-][0-9]*[-][0-9]*");
+            String strValidRealPattern = "^([-]|[.]|[-.]|[0-9])[0-9]*[.]*[0-9]+$";
+            String strValidIntegerPattern = "^([-]|[0-9])[0-9]*$";
+            Regex objNumberPattern = new Regex("(" + strValidRealPattern + ")|(" + strValidIntegerPattern + ")");
+            return !objNotNumberPattern.IsMatch(strNumber) &&
+                   !objTwoDotPattern.IsMatch(strNumber) &&
+                   !objTwoMinusPattern.IsMatch(strNumber) &&
+                   objNumberPattern.IsMatch(strNumber);
+        }
+
+        /// <summary>
+        ///  判断是否是日期字符串
+        /// </summary>
+        /// <param name="in_str"></param>
+        /// <returns></returns>
+        public static bool IsDateStr_yyyymmdd(string in_str)
+        {
+            if (in_str == "") return true;
+            if (in_str.Length != 8) return false;
+            return IsDateStr(in_str);
+        }
+
+        /// <summary>
+        /// 判断是否是日期字符串
+        /// </summary>
+        /// <param name="in_str"></param>
+        /// <returns></returns>
+        public static bool IsDateStr(string in_str)
+        {
+            if (in_str == "") return true;
+            if (in_str.Length == 8)
+                in_str = in_str.Substring(0, 4) + "-" + in_str.Substring(4, 2) + "-" + in_str.Substring(6, 2);
+            DateTime dtDate;
+            bool bValid = true;
+            try
+            {
+                dtDate = DateTime.Parse(in_str);
+            }
+            catch (FormatException)
+            {
+                // 如果解析方法失败则表示不是日期性数据
+                bValid = false;
+            }
+            return bValid;
+        }
+
+        /// <summary>
+        /// 判断字符串中是否包含汉字,有返回true 否则为false
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsExistHanZi(string str)
+        {
+            Regex reg = new Regex(@"[\u4e00-\u9fa5]");//正则表达式
+            if (reg.IsMatch(str))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 字段串是否为Null或为""(空)
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsStrNullOrEmpty(string str)
+        {
+            if (str == null || str.Trim() == string.Empty)
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// 返回文件是否存在
+        /// </summary>
+        /// <param name="filename">文件名</param>
+        /// <returns>是否存在</returns>
+        public static bool IsFileExists(string filename)
+        {
+            return System.IO.File.Exists(filename);
+        }
+
+
+        /// <summary>
+        /// 检测是否符合email格式
+        /// </summary>
+        /// <param name="strEmail">要判断的email字符串</param>
+        /// <returns>判断结果</returns>
+        public static bool IsValidEmail(string strEmail)
+        {
+            return Regex.IsMatch(strEmail, @"^[\w\.]+([-]\w+)*@[A-Za-z0-9-_]+[\.][A-Za-z0-9-_]");
+        }
+
+        public static bool IsValidDoEmail(string strEmail)
+        {
+            return Regex.IsMatch(strEmail, @"^@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+        }
+        /// <summary>
+        /// 检测是否是正确的Url
+        /// </summary>
+        /// <param name="strUrl">要验证的Url</param>
+        /// <returns>判断结果</returns>
+        public static bool IsURL(string strUrl)
+        {
+            return Regex.IsMatch(strUrl, @"^(http|https)\://([a-zA-Z0-9\.\-]+(\:[a-zA-Z0-9\.&%\$\-]+)*@)*((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|localhost|([a-zA-Z0-9\-]+\.)*[a-zA-Z0-9\-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{1,10}))(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\?\'\\\+&%\$#\=~_\-]+))*$");
+        }
+
+        /// <summary>
+        /// 判断是否为base64字符串
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static bool IsBase64String(string str)
+        {
+            //A-Z, a-z, 0-9, +, /, =
+            return Regex.IsMatch(str, @"[A-Za-z0-9\+\/\=]");
+        }
+
+        /// <summary>
+        /// 检测是否有Sql危险字符
+        /// </summary>
+        /// <param name="str">要判断字符串</param>
+        /// <returns>判断结果</returns>
+        public static bool IsSafeSqlString(string str)
+        {
+            return !Regex.IsMatch(str, @"[-|;|,|\/|\(|\)|\[|\]|\}|\{|%|@|\*|!|\']");
+        }
+    }
+}
---
+++ Tools.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Text;
+
+namespace SGBDynamoLibrary
+{
+    public class Tools
+    {
+        public static string InvokeCmd(string cmdArgs)
+        {
+            string Tstr = "";
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+
+            p.StandardInput.WriteLine(cmdArgs);
+            p.StandardInput.WriteLine("exit");
+            Tstr = p.StandardOutput.ReadToEnd();
+            p.Close();
+            return Tstr;
+        }
+
+        public static bool IsCurrentUserAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                return true;
+            }
+            else
+            {
+                //MessageBox.Show("请以管理员身份运行!");
+                return false;
+            }
+
+        }
+    }
+}
